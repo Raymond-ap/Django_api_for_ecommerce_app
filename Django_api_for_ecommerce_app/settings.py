87c3cb42338f11ff887acb63ed2a1b6994dbeb45
raymond-ap <apunguray@gmail.com>
2021-07-30 14:15:06 -0700

    commit

--- Django_api_for_ecommerce_app/settings.py
+++ Django_api_for_ecommerce_app/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'api.apps.ApiConfig',
+    'product.apps.ProductConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
