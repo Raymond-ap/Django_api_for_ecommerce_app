d07accaa225dfbd749afebebd9bf8a25bc09921f
raymond-ap <apunguray@gmail.com>
2021-07-30 14:14:45 -0700

    Category model

---
+++ product/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Category(models.Model):
+    category = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_plural_name = "Categories"
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.category
+
+
