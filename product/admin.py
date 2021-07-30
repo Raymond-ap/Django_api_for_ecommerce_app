5f594df9592a382425f5d59921edd1e67314a1eb
raymond-ap <apunguray@gmail.com>
2021-07-30 14:34:57 -0700

    commit

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class ProductImageInline(admin.TabularInline):
+    models = ProductImage
+    extra = 3
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [ProductImageInline]
+    list_display = (
+        'product_name',
+        'category',
+        'price',
+        'published',
+        'created',
+    )
+    search_fields = (
+        'product_name',
+        'category',
+        'price'
+    )
