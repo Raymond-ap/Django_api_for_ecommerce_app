eaed00edf65d9c496678fcfd64af1601e42f0a1a
raymond-ap <apunguray@gmail.com>
2021-07-30 14:58:36 -0700

    commit

--- api/views.py
+++ api/views.py
@@ -1,3 +1,10 @@
+from django.http import JsonResponse, request
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET', 'POST'])
+def APIOVERVIEW(request):
+    pass
