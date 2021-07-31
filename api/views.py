8845f4d3ef3b2ecc049dde4b1ea17e8cbdbdb692
raymond-ap <apunguray@gmail.com>
2021-07-31 03:35:14 -0700

    commit

--- api/views.py
+++ api/views.py
@@ -5,6 +5,27 @@ from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from product.models import Product, Category
+from product.serializers import ProductSerializer
+
 @api_view(['GET', 'POST'])
 def APIOVERVIEW(request):
-    pass
+    api_urls = {
+        "Products": "/products_all",
+        "Single Product Detail": "/product_detail/id/"
+    }
+    return JsonResponse(api_urls)
+
+
+@api_view(["GET", "POST"])
+def PRODUCTSVIEW(request):
+    products = Product.objects.filter(published=True);
+    serializers = ProductSerializer(products, many=True);
+    return Response(serializers.data)
+
+
+@api_view(["GET", "POST"])
+def PRODUCTDETAIL(request, id):
+    product = Product.objects.get(id=id, published=True)
+    serializers = ProductSerializer(product, many=False)
+    return Response(serializers.data)
